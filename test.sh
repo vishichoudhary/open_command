6058f2b5722dc33c8b2ab9947e259701074e3918
vishichoudhary <vishal129.vk@gmail.com>
2017-07-03 08:48:12 +0530

    deleted some raw file

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-size=""
-$size | `ls`
-echo $size
-#main="mysql -u root --password="urmilaa" -e "
-#$main "use data_open; insert into data_open_table(ext_name,app1) values('mp3','vlc');"
-echo $?
